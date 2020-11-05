7cde8cb2698214b2bd7385dd088fa80b60d96df9
Audie Milson <audie.535180021@stu.untar.ac.id>
2020-11-05 14:16:04 +0700

    pertemuan 8 Node JS

--- Pertemuan 8 NodeJS/code/routing/dynamicview.js
+++ Pertemuan 8 NodeJS/code/routing/dynamicview.js
@@ -3,18 +3,20 @@ const http = require('http');
 const httpStatus = require('http-status-codes');
 const fs = require('fs');
 
-const getViewUrl = (url)=>{
+const getViewUrl = (url) => {
     return `views/${url}.html`;
 }
 
-http.createServer((req, res)=>{
+http.createServer((req, res) => {
     let ViewUrl = getViewUrl(req.url);
-    fs.readFile(ViewUrl, (error, data)=>{
-        if(error){
+    fs.readFile(ViewUrl, (error, data) => {
+        if (error) {
             res.writeHead(httpStatus.NOT_FOUND);
             res.write("<h1>FILE NOT FOUND</h1>");
-        }else{
-            res.writeHead(httpStatus.OK, {"Content-Type":"text/html"});
+        } else {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/html"
+            });
             res.write(data);
         }
         res.end();
